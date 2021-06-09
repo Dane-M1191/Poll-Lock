0ad5b075a612c7acd7ce77e9cb2c4abf3ad26c49
booma1191 <booma1191.m@gmail.com>
2021-06-09 05:04:05 -0500

    Created a CategoryRepository interface.

---
+++ src/main/java/PollPoint/data/CategoryRepository.java
@@ -0,0 +1,7 @@
+package PollPoint.data;
+
+import PollPoint.models.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+}
